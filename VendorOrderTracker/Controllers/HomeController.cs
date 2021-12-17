e4c3542635a6e2231a3e9afdd603c5536a483c25
Matt Luker <jamesmattluker@gmail.com>
2021-12-17 14:40:41 -0700

    add Index View and HomeController

---
+++ VendorOrderTracker/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {     
+      return View();
+    }
+  }
+}
