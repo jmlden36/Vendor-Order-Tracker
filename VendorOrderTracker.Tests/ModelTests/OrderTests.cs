ada867bf8421b21a497b89a39bce589db1df5d50
Matt Luker <jamesmattluker@gmail.com>
2021-12-17 11:55:35 -0700

    add test 13 for Order.cs and code to pass

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -147,5 +147,26 @@ namespace VendorOrderTracker.Tests
 
       Assert.AreEqual(1, result);
     }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      string name01 = "Vendor1";
+      string descripton01 = "Vendor1 description";
+      double price01 = 12.92;
+      string date01 = "2000-02-02";
+
+      string name02 = "Vendor2";
+      string descripton02 = "Vendor2 description";
+      double price02 = 20;
+      string date02 = "2003-03-03";
+
+      Order newOrder01 = new Order(name01, descripton01, price01, date01);
+      Order newOrder02 = new Order(name02, descripton02, price02, date02);
+
+      Order result = Order.Find(2);
+
+      Assert.AreEqual(newOrder02, result);
+    }
   }
 }
--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -28,5 +28,10 @@ namespace VendorOrderTracker.Models
     {
       return _instances;
     }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
