46f4aa5dc032c692bd52ef30581f83ad33a1da82
Matt Luker <jamesmattluker@gmail.com>
2021-12-17 12:35:56 -0700

    add test 1 for Vendor.cs and code to pass

---
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Vendor1", "Vendor1 description");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
